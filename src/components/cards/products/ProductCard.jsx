d26378b9b2390b06454196b04b8d48bbf68ddeed
Jay Hansea <ezeokafortochukwu@gmail.com>
2023-10-04 12:10:59 +0100

    Created products card

--- src/components/cards/products/ProductCard.jsx
+++ src/components/cards/products/ProductCard.jsx
@@ -1,10 +1,54 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, Pressable, Image } from "react-native";
+import styles from "./ProductCard.Style";
+import ReusableText from "../../reusables/text/ReusableText";
+import { SIZES, COLORS } from "../../../constants/theme";
+import { MaterialIcons } from "@expo/vector-icons";
 
-const ProductCard = () => {
+const ProductCardItem = ({ product }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
-    <View>
-      <Text>ProductCard</Text>
+    <Pressable style={styles.card}>
+      <View style={styles.favoriteContainer}>
+        <Pressable onPress={toggleFavorite}>
+          <MaterialIcons
+            name={isFavorite ? "favorite" : "favorite-outline"}
+            size={24}
+            color={COLORS.red}
+          />
+        </Pressable>
+      </View>
+      <Image
+        source={product.product_photo}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.title}>
+        <ReusableText
+          numberOfLines={2}
+          style={styles.title}
+          text={product.product_title}
+          fontSize={SIZES.medium}
+        />
+      </View>
+      <View style={styles.priceView}>
+        <Text style={styles.productPrice}>{product.product_price}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const ProductCard = ({ products }) => {
+  return (
+    <View style={styles.cardContainer}>
+      {products.map((product, index) => (
+        <ProductCardItem key={index} product={product} />
+      ))}
     </View>
   );
 };
