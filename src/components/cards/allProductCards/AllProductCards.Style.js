e0e4ef291175c62733f789eb9f2bf182f111f88c
Jay Hansea <ezeokafortochukwu@gmail.com>
2023-11-02 21:54:19 +0100

    made product item card a component

--- src/components/cards/allProductCards/AllProductCards.Style.js
+++ src/components/cards/allProductCards/AllProductCards.Style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { COLORS, SIZES, SHADOWS } from "../../../constants/theme";
+import { SIZES } from "../../../constants/theme";
 
 const styles = StyleSheet.create({
   cardContainer: {
@@ -8,35 +8,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: SIZES.xSmall,
     marginTop: SIZES.medium,
-  },
-  card: {
-    width: "48%", // Set to 48% to allow for a small gap between the cards
-    marginBottom: SIZES.medium,
-    padding: SIZES.medium,
-    backgroundColor: COLORS.white,
-    borderRadius: SIZES.medium,
-    ...SHADOWS.medium,
-  },
-  favoriteContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    paddingBottom: SIZES.xSmall,
-  },
-  image: {
-    width: "100%", // Take up the entire width of the card
-    height: 80,
-  },
-  title: {
-    marginTop: SIZES.medium,
-  },
-  priceView: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginVertical: SIZES.small,
-  },
-  productPrice: {
-    color: COLORS.green,
-    fontSize: SIZES.large,
   },
 });
 
---
+++ src/components/cards/productCardItem/ProductCardItem.jsx
@@ -0,0 +1,46 @@
+import { View, Text, Pressable, Image } from "react-native";
+import React, { useState } from "react";
+import styles from "./ProductCardItem.Style";
+import ReusableText from "../../reusables/text/ReusableText";
+import { SIZES, COLORS } from "../../../constants/theme";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const ProductCardItem = ({ product }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <Pressable style={styles.card}>
+      <View style={styles.favoriteContainer}>
+        <Pressable onPress={toggleFavorite}>
+          <MaterialIcons
+            name={isFavorite ? "favorite" : "favorite-outline"}
+            size={24}
+            color={COLORS.red}
+          />
+        </Pressable>
+      </View>
+      <Image
+        source={product.product_photo}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.title}>
+        <ReusableText
+          numberOfLines={2}
+          style={styles.title}
+          text={product.product_title}
+          fontSize={SIZES.medium}
+        />
+      </View>
+      <View style={styles.priceView}>
+        <Text style={styles.productPrice}>{product.product_price}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default ProductCardItem;
