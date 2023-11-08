425dbced71f971f685e18228478183b430f99c89
Jay Hansea <ezeokafortochukwu@gmail.com>
2023-11-08 22:17:24 +0100

    implemented cartSlice with redux toolkit

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -5,8 +5,11 @@ import reusable from "../../components/reusables/styles/Reusable.Style";
 import Welcome from "../../components/home/welcome/Welcome";
 import Sales from "../../components/home/salesProduct/Sales";
 import Products from "../../components/home/products/Products";
+import { useSelector } from "react-redux";
 
 const Home = ({ navigation }) => {
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  console.log(cartItems);
   return (
     <ScrollView>
       <SafeAreaView style={[reusable.container, styles.container]}>
